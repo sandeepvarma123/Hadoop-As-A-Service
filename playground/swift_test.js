956845ce9cf28b441bf45aa46d81a8594ce30960
sandeepvarma <vatsavayi.sandeep@gmail.com>
2019-03-25 12:34:23 +0530

    Added Swift Test

--- playground/swift_test.js
+++ playground/swift_test.js
@@ -1,32 +1,53 @@
 var request = require('request');
 const fs = require('fs');
 
+/*
+var body = {"job_binary": {"name": "hadoop1.jar", "url": "swift://scripts/hadoop1.jar", "tenant_id": "109d5a0fef34423582747e609b8c6c0f", "created_at": "2019-03-23T07:16:41", "is_protected": false, "is_public": false, "id": "2e6350ce-6cbe-4600-9e85-f580d0497443"}};
+//body = JSON.parse(body);
+console.log(body["job_binary"]["id"]);
+*/
+
+
 
 function swift_container(token){
-
- /* request.get({
-    url : 'http://172.16.2.140:8080/v1/AUTH_109d5a0fef34423582747e609b8c6c0f',
-    headers: {
-      'Content-Type':'application/json',
-       'X-Auth-Token' : token
-    }
-  },(err,response,body)=>{
-      console.log(body);
-  });*/
-
-  fs.createReadStream('helloworld.rar').pipe(request.put({
-    url: 'http://172.16.2.140:8080/v1/AUTH_109d5a0fef34423582747e609b8c6c0f/scripts/helloworld.rar',
-    headers: {
-      'Content-Type':'application/json',
-       'X-Auth-Token' : token
-    }
-  },(err,response,body)=>{
-      if(err){
-        console.log(err);
+  
+  //upload to swift container
+  fs.createReadStream('hadoop.jar').pipe(request.put({
+      url: 'http://172.16.2.140:8080/v1/AUTH_109d5a0fef34423582747e609b8c6c0f/scripts/hadoop1.jar',
+      headers: {
+        'Content-Type':'application/json',
+        'X-Auth-Token' : token
       }
-      console.log(response.statusMessage);
-  })
-)
+    },(err,response,body)=>{
+        if(err){
+          console.log(err);
+        }
+        console.log(response.statusMessage);
+    })
+  ) 
+
+  //create job binary
+  request.post({
+    url: 'http://172.16.2.140:8386/v1.1/109d5a0fef34423582747e609b8c6c0f/job-binaries',
+    headers: {
+      'Content-Type':'application/json',
+      'X-Auth-Token' : token
+    },
+    body:  JSON.stringify({
+      "url": "swift://scripts/hadoop1.jar",
+      "name": "hadoop1.jar",
+      "extra": {
+        "password": "ghost0197",
+        "user": "admin"
+      }
+    })
+  },(err,response,body)=>{
+       console.log(body);
+       
+  });
+
+  //list job binaries and get ID
+
 
 }
 
