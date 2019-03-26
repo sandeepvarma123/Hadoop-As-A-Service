74f2df9f9b3ed59bb8658a70f0e4970ddc040efe
GvsHarsha <harshagvs123@gmail.com>
2019-03-26 14:05:28 +0530

    updated with multiple file upload

--- routes/Upload.js
+++ routes/Upload.js
@@ -5,32 +5,57 @@ const router = express.Router();
 //var app = Express(); 
 //app.use(bodyParser.json()); 
 
-var Storage = multer.diskStorage({
+var Storage1 = multer.diskStorage({
     destination: function (req, file, callback) {
         callback(null, "./public/uploads");
     },
     filename: function (req, file, callback) {
         console.log(file.originalname);
-        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+        callback(null, file.originalname);
     }
 });
 
-var upload = multer({ storage: Storage }).array("imgUploader", 1); //Field name and max count 
+/*var Storage2 = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, "./public/uploads");
+    },
+    filename: function (req, file, callback) {
+        console.log(file.originalname);
+        callback(null, file.originalname);
+    }
+});*/
+
+var upload1 = multer({ storage: Storage1 }).array("imgUploader", 2); //Field name and max count 
+//var upload2 = multer({ storage: Storage1 }).array("imgUploader2", 1);
 
 //array(fieldname[, maxCount]);
 
-router.get("/Upload", (req, res) =>  res.render('fileupload')
-    //res.sendFile(__dirname + "/index.html"); 
-); 
+router.get("/Upload", (req, res) =>  {
+    
+    res.render('fileupload');
+}); 
   
 router.post("/Upload", function (req, res) { 
-    upload(req, res, function (err) { 
+    console.log(req.body);
+    upload1(req, res, function (err) { 
         if (err) { 
             console.log(err);
-            return res.end("Something went wrong!"); 
+            return res.end("Something went wrong in 1!"); 
         } 
-        return res.end("File uploaded sucessfully!."); 
+
+       /* upload2(req, res, function (err) { 
+            if (err) { 
+                console.log(err);
+                return res.end("Something went wrong in 2!"); 
+            } 
+            
+           // return res.end("File2 uploaded sucessfully!."); 
+        }); */
+        
+        return res.end("Files uploaded sucessfully!."); 
     }); 
-}); 
+   
+}
+); 
 
 module.exports = router;
